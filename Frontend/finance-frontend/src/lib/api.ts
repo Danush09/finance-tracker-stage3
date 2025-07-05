dc1904756c25f6db0430a983f8456f2a89362f2f
Danush09 <136923185+Danush09@users.noreply.github.com>
2025-07-05 13:31:04 +0530

    Update api.ts

--- Frontend/finance-frontend/src/lib/api.ts
+++ Frontend/finance-frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/transactions";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL!;
 
 export const getTransactions = () => axios.get(BASE_URL);
 export const addTransaction = (data: any) => axios.post(BASE_URL, data);
